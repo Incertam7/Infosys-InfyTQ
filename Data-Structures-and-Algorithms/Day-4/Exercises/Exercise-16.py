ed655ea87f8eeb4268f6f5be161e335aac244cc2
Incertam7 <shreyas.yakhob@gmail.com>
2020-04-15 13:29:42 +0530

    Create Exercise-16.py

---
+++ Data-Structures-and-Algorithms/Day-4/Exercises/Exercise-16.py
@@ -0,0 +1,13 @@
+#DSA-Exer-16
+
+def find_decreasing_start(list1, start, end):
+    for i in range(0, len(list1)):
+        if list1[i] >= list1[i + 1]:
+            return i + 1
+
+#Use different values for list1 and test your program
+list1 = [1,4,7,8,9,5,4]
+start = 0
+end = len(list1) - 1
+result = find_decreasing_start(list1, start, end)
+print("The index position at which the increasing array starts decreasing is:", result)
