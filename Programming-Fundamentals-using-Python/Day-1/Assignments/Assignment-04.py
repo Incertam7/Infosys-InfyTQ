e47adb49cc7816ed43c84ccc7abfd08efe2f44c5
Incertam7 <shreyas.yakhob@gmail.com>
2020-04-22 08:18:47 +0530

    Create Assignment-04.py

---
+++ Programming-Fundamentals-using-Python/Day-1/Assignments/Assignment-04.py
@@ -0,0 +1,15 @@
+#PF-Assgn-4
+#This verification is based on string match.
+
+principal = 7800
+rate_of_interest = 7.7
+time = 26
+interest = 0
+
+#Start writing your code from here
+#Populate the variable: interest
+interest = (principal * time * rate_of_interest) / 100
+
+
+#Do not modify the below print statement for verification to work
+print(interest)
