d0cf7e47610332e037cfbbb1ec0ac1c63939560a
Shreyas Yakhob <shreyas.yakhob@gmail.com>
2020-07-06 05:55:42 +0530

    Create Assignment-29.py

---
+++ Programming-Fundamentals-using-Python/Day-4/Assignments/Assignment-29.py
@@ -0,0 +1,15 @@
+//JS-Exer-25
+
+//Start writing your code here
+function find_sum(num)
+{
+    var sum = 0
+    for(int i = 1; i <= num; i++)
+    {
+        sum += i
+    }
+    
+    return sum
+}
+
+console.log(find_sum(6))
