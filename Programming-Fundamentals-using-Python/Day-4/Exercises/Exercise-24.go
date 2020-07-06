7dc9c5ea7a8b702026cb7c148f9d3349fc660019
Shreyas Yakhob <shreyas.yakhob@gmail.com>
2020-07-06 05:51:58 +0530

    Create Exercise-24.go

---
+++ Programming-Fundamentals-using-Python/Day-4/Exercises/Exercise-24.go
@@ -0,0 +1,14 @@
+//PF-Exer-24
+//This verification is based on string match.
+
+package main
+
+import("fmt")
+func main() {
+    //Write your program logic here
+    fmt.Println(square(3))
+}
+
+func square(num int) int{
+    return num * num
+}
